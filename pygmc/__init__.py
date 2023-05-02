9976b574d0bc2cf8f41f695fe711cdec0114e60e
Thomaz <thomazwrx@gmail.com>
2023-05-02 15:40:08 -0700

    Added ETH donation address Only interested in Ethereum, convert buttcoin to donate

--- pygmc/__init__.py
+++ pygmc/__init__.py
@@ -1,4 +1,5 @@
 __version__ = "0.0.1-dev"
-__author__ = "Thomaz Santana"
+__author__ = "Thomaz Lago Santana"
 __email__ = "thomazwrx@gmail.com"
 __license__ = "MIT"
+__eth__ = "0x2F2ecef407860815b315a2ecC482b4c459921974"  # Ethereum donation address thomaz.eth
