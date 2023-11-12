bb33cc22f9ead6c52675317fdeb8f33aa89fbdec
Thomaz <zoidberg137@gmail.com>
2023-11-11 17:07:30 -0800

    Added ruff config - Added ruff config - removed setup.py - made pyproject.toml the build source

--- tasks.py
+++ tasks.py
@@ -4,3 +4,14 @@ from invoke import task
 @task
 def test(ctx):
     ctx.run("pytest --cov=pygmc .")
+
+
+@task
+def bugbear(ctx):
+    # run flake8-bugbear
+    ctx.run("ruff check --no-fix . --select=B")
+
+
+@task
+def build(ctx):
+    ctx.run("python -m build")
