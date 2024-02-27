5de153cd445744ff119347fbd7544be090acb133
huyndao <40338813+huyndao@users.noreply.github.com>
2024-02-26 19:14:52 -0500

    added mock connection for device cmd response map

--- tests/test_devices/test_gmcse.py
+++ tests/test_devices/test_gmcse.py
@@ -3,6 +3,7 @@ import types
 import pytest
 
 import pygmc
+from pygmc import devices
 
 from ..data import data_gmcse
 from ..mocks import MockConnection
@@ -11,6 +12,13 @@ actions_conn = MockConnection(data_gmcse.actions_cmd_response_map)
 gcse = pygmc.GMCSE(port=None, baudrate=123, connection=actions_conn)
 
 parametrize_data = [(gcse, x) for x in data_gmcse.actions_device_test_cases]
+
+# Mock the connection to a device
+# Uses recorded responses device
+mock_connection = MockConnection(data_gmcse.cmd_response_map)
+
+# Use our fake/mock connection in our real device class
+mock_device = devices.DeviceRFC1201(mock_connection)
 
 
 @pytest.mark.parametrize("gc,case", parametrize_data)
@@ -29,3 +37,14 @@ def test_expected_results(gc, case):
         response = next(iter(response))
     assert response == case["return"]
     assert actions_conn.get_cmd_calls(case["expected_write"]) == expected_calls
+
+
+def test_auto_device():
+    device = devices.auto_get_device_from_connection(mock_connection)
+    assert isinstance(type(device), type(devices.GMCSE))
+
+
+def test_reset_buffers(capfd):
+    mock_device.get_config()
+    out, err = capfd.readouterr()
+    assert "reset_buffers" in out
