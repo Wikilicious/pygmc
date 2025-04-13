ea1ac13ad98bbc9755af655c3b1673fd22724fe4
Thomaz <thomazwrx@gmail.com>
2025-04-13 14:59:06 -0700

    Added GMC320S import

--- pygmc/devices/__init__.py
+++ pygmc/devices/__init__.py
@@ -9,7 +9,7 @@ from .device_rfc1201 import DeviceRFC1201
 from .device_rfc1801 import DeviceRFC1801
 from .device_spec404 import DeviceSpec404
 from .gmc300 import GMC300, GMC300S, GMC300EPlus
-from .gmc320 import GMC320, GMC320Plus, GMC320PlusV5
+from .gmc320 import GMC320, GMC320S, GMC320Plus, GMC320PlusV5
 from .gmc500 import GMC500, GMC500Plus
 from .gmc600 import GMC600, GMC600Plus
 from .gmc800 import GMC800
